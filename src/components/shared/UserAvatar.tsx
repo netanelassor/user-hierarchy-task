a42b4928ff4e95e43f7d3e72944afb5ccbb40f93
Nati Assor <nassor@salesforce.com>
2024-07-25 08:52:41 +0300

    handleImageError

--- src/components/shared/UserAvatar.tsx
+++ src/components/shared/UserAvatar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type UserAvatarProps = {
   photo: string;
   firstName: string;
@@ -9,22 +11,26 @@ export default function UserAvatar({
   lastName,
   photo,
 }: UserAvatarProps): JSX.Element {
+  const [showInitial, setShowInitial] = useState(false);
+
+  function handleImageError(): void {
+    setShowInitial(true);
+  }
 
   return (
     <div className="flex flex-wrap items-center justify-center gap-3">
-      <div className="h-10 w-10">     
-        {photo ? (
+      <div className="h-10 w-10">
+        {showInitial ? (
+          <div className="flex items-center text-center justify-center text-md h-full w-full rounded-full ring ring-indigo-600 bg-indigo-600">
+            <div className="w-full">{`${firstName[0]}${lastName[0]}`}</div>
+          </div>
+        ) : (
           <img
             className="h-full w-full rounded-full object-cover ring ring-indigo-600 object-center bg-indigo-600"
+            onError={handleImageError}
             src={photo}
             alt={`${lastName} ${lastName}`}
           />
-        ) : (
-          <div className="flex items-center text-center justify-center text-md h-full w-full rounded-full ring ring-indigo-600 bg-indigo-600">
-            <div className="w-full">
-            {`${firstName[0]}${lastName[0]}`}
-            </div>
-          </div>
         )}
       </div>
     </div>
