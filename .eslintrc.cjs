c413491cd6dfffff1ec366e85e5c9181b68b97a4
Nati Assor <nassor@salesforce.com>
2024-07-31 00:23:53 +0300

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     "plugin:react/recommended",
-    "plugin:react-hooks/recommended"
+    "plugin:react-hooks/recommended",
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
